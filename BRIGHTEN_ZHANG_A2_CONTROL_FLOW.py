ab0722b472e1fcac497b52364a673813f6543df9
NeverQuitYourDayDream <78627242+NeverQuitYourDayDream@users.noreply.github.com>
2021-02-17 18:58:31 -0500

    tweaks

--- BRIGHTEN_ZHANG_A2_CONTROL_FLOW.py
+++ BRIGHTEN_ZHANG_A2_CONTROL_FLOW.py
@@ -1,13 +1,16 @@
 # Quadratic Equation Solver
-# QES finds the real roots of Second-degree polynomial functions of the form ax^2 + bx + c = 0, 
+# QES finds the real roots of quadratic equations
+# of the form ax^2 + bx + c = 0,
 # with a being a non-zero integer
 # Created on 2/17/2021 by Brighten Zhang
 
 import math
 from math import sqrt
 
-print("Quadratic Equation Solver")
-print("QES finds the real roots of Second-degree\nequations of the form ax^2 + bx + c = 0")
+print("-------------------------------------------------------")
+print("============== Quadratic Equation Solver ==============")
+print("-------------------------------------------------------")
+print("QES finds the real roots of Quadratic equations of the \nform ax^2 + bx + c = 0")
 
 another = 'y'
 while another == 'y':
@@ -15,7 +18,7 @@ while another == 'y':
     # get values of a, b, c from user input
     a, b, c = map(
         float,
-        input("Enter space-seperated values for a, b, and c: ").split())
+        input("\nEnter space-seperated values for a, b, c: ").split())
 
     # print the equation so that users can see
     # what equation we're dealing with
@@ -42,11 +45,13 @@ while another == 'y':
         else:
             result.append('')
 
-    print("The equation is: ")
+	# print the equation
     if b != 0:
-        print("{0}x^2 {1}x {2} = 0".format(result[0], result[1], result[2]))
+        print("\nThe equation is {0}x^2 {1}x {2} = 0\n".format(
+            result[0], result[1], result[2]))
     else:
-        print("{0}x^2 {1} = 0".format(result[0], result[2]))
+        print("\nThe equation is {0}x^2 {1} = 0\n".format(
+            result[0], result[2]))
 
     # discriminant D = b^2 - 4ac
     # if D > 0 then there are 2 real solutions
@@ -72,17 +77,23 @@ while another == 'y':
     # let x1 be first soltion taking +
     # let x2 be second solution taking -
     if real_roots == 2:
-        x1 = round((-b + sqrt(b * b - 4 * a * c)) / (2 * a), 2)
-        x2 = round((-b - sqrt(b * b - 4 * a * c)) / (2 * a), 2)
+        x1 = round((-b + sqrt(b * b - 4 * a * c)) / (2 * a), 3)
+        x2 = round((-b - sqrt(b * b - 4 * a * c)) / (2 * a), 3)
+        if int(x1) == x1:
+            x1 = int(x1)
+        if int(x2) == x2:
+            x2 = int(x2)
         print("The two solutions are\nx = {0} and \nx = {1}".format(x1, x2))
     elif real_roots == 1:
         # D = 0
-        x = round((-b) / (2 * a), 2)
-        print("The solution is ", x)
+        x = round((-b) / (2 * a), 3)
+        if int(x) == x:
+            x = int(x)
+        print("The solution is x = {}".format(x))
     else:
         # there are no solutions so we're done
         pass
 
-    another = input("Enter another equation? y/n: ")
+    another = input("\nEnter another equation? y/n: ")
     if another == 'n':
-        print("Thank you, have a nice day.")
+        print("\nThank you for using this calculator, have a nice day.")
